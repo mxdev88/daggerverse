c4d42ead0e999eaaef2ccf4eb621070283f3dd36
mxdev88 <mxdev88@gmail.com>
2024-08-18 15:55:45 +0200

    feat: add first scrapyd implementation

--- scrapyd/src/main/__init__.py
+++ scrapyd/src/main/__init__.py
@@ -1,43 +1,55 @@
-"""A generated module for Scrapyd functions
-
-This module has been generated via dagger init and serves as a reference to
-basic module structure as you get started with Dagger.
-
-Two functions have been pre-created. You can modify, delete, or add to them,
-as needed. They demonstrate usage of arguments and return types using simple
-echo and grep commands. The functions can be called from the dagger CLI or
-from one of the SDKs.
-
-The first line in this comment block is a short description line and the
-rest is a long description with more detail on the module's purpose or usage,
-if appropriate. All modules should have a short description.
-"""
+"""Module for running scrapyd"""
 
 import dagger
 from dagger import dag, function, object_type
-
-# NOTE: it's recommended to move your code into other files in this package
-# and keep __init__.py for imports only, according to Python's convention.
-# The only requirement is that Dagger needs to be able to import a package
-# called "main", so as long as the files are imported here, they should be
-# available to Dagger.
 
 
 @object_type
 class Scrapyd:
     @function
-    def container_echo(self, string_arg: str) -> dagger.Container:
-        """Returns a container that echoes whatever string argument is provided"""
-        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
+    def base_env(
+        self,
+        source: dagger.Directory,
+        url: str | None,
+        username: str | None,
+        password: dagger.Secret | None,
+        ssh: dagger.Directory | None,
+    ) -> dagger.Container:
+        """Build base environment"""
+        ctr = (
+            dag.container()
+            .from_("python:3.11-bookworm")
+            .with_directory("/src", source)
+            .with_workdir("/src")
+            .with_exec(["pip", "install", "scrapyd-client"])
+        )
+
+        if ssh:
+            ctr = ctr.with_directory("/root/.ssh", ssh).with_exec(
+                ["chmod", "-R", "400", "/root/.ssh"]
+            )
+        if url:
+            ctr = ctr.with_env_variable("SCRAPYD_URL", url)
+        if username:
+            ctr = ctr.with_env_variable("SCRAPYD_USERNAME", username)
+        if password:
+            ctr = ctr.with_secret_variable("SCRAPYD_PASSWORD", password)
+        return ctr
 
     @function
-    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
-        """Returns lines that match a pattern in the files of the provided Directory"""
-        return await (
-            dag.container()
-            .from_("alpine:latest")
-            .with_mounted_directory("/mnt", directory_arg)
-            .with_workdir("/mnt")
-            .with_exec(["grep", "-R", pattern, "."])
-            .stdout()
-        )
+    async def deploy(
+        self,
+        source: dagger.Directory,
+        include_dependencies: bool = True,
+        url: str | None = None,
+        username: str | None = None,
+        password: dagger.Secret | None = None,
+        ssh: dagger.Directory | None = None,
+    ) -> dagger.Container:
+        args = []
+        if include_dependencies:
+            args.append("--include-dependencies")
+
+        return await self.base_env(
+            source, url=url, username=username, password=password, ssh=ssh
+        ).with_exec(["scrapyd-deploy", *args])
