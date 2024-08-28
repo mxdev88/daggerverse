bac74972ac3af5d53ecdb7d51a82cff5934f760f
mxdev88 <mxdev88@gmail.com>
2024-08-28 20:43:56 +0200

    feat(scrapyd): add ssh-sock

--- scrapyd/src/main/__init__.py
+++ scrapyd/src/main/__init__.py
@@ -14,6 +14,7 @@ class Scrapyd:
         username: str | None,
         password: dagger.Secret | None,
         ssh: dagger.Directory | None,
+        ssh_sock: dagger.Socket | None,
     ) -> dagger.Container:
         """Build base environment"""
         ctr = (
@@ -25,9 +26,18 @@ class Scrapyd:
         )
 
         if ssh:
-            ctr = ctr.with_directory("/root/.ssh", ssh).with_exec(
-                ["chmod", "-R", "400", "/root/.ssh"]
+            ctr = (
+                ctr.with_directory("/root/.ssh", ssh)
+                .with_exec(["chmod", "700", "/root/.ssh"])
+                .with_exec(["chmod", "600", "/root/.ssh/*"])
+                .with_exec(["chmod", "644", "/root/.ssh/known_hosts"])
+                .with_exec(["ssh-add", "/root/.ssh/*"])
             )
+        if ssh_sock:
+            ctr = ctr.with_unix_socket("/tmp/ssh.sock", ssh_sock).with_env_variable(
+                "SSH_AUTH_SOCK", "/tmp/ssh.sock"
+            )
+
         if url:
             ctr = ctr.with_env_variable("SCRAPYD_URL", url)
         if username:
@@ -45,11 +55,12 @@ class Scrapyd:
         username: str | None = None,
         password: dagger.Secret | None = None,
         ssh: dagger.Directory | None = None,
+        ssh_sock: dagger.Socket | None = None,
     ) -> dagger.Container:
         args = []
         if include_dependencies:
             args.append("--include-dependencies")
 
         return await self.base(
-            source, url=url, username=username, password=password, ssh=ssh
+            source, url=url, username=username, password=password, ssh=ssh, ssh_sock=ssh_sock
         ).with_exec(["scrapyd-deploy", *args])
