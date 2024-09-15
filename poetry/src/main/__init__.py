bf4b8a864d0322930946b5f797b324c1f72ae023
mxdev88 <mxdev88@gmail.com>
2024-09-15 11:40:16 +0200

    feat: add option to choose poetry version

--- poetry/src/main/__init__.py
+++ poetry/src/main/__init__.py
@@ -1,31 +1,34 @@
 """Module for running Poetry commands."""
 
+import dataclasses
 import dagger
 from dagger import dag, function, object_type
 
 
 @object_type
 class Poetry:
-    @function
-    def base(
-        self,
-        version: str | None = None,
-    ) -> dagger.Container:
-        """Build base environment"""
-        pinned_version = ""
+    ctr: dagger.Container = dataclasses.field(init=False)
+    version: dataclasses.InitVar[str | None] = None
+
+    def __post_init__(self, version: str | None = None):
+        poetry_version = ""
         if version:
-            pinned_version = f"--version {version}"
+            poetry_version = f"--version {version}"
 
         # curl -sSL https://install.python-poetry.org | python3 - --version 1.2.0
-        ctr = (
+        self.ctr = (
             dag.container()
             .from_("python:3.11-bookworm")
             .with_exec(
                 [
                     "sh",
                     "-c",
-                    f"curl -sSL https://install.python-poetry.org | python3 - {pinned_version}",
+                    f"curl -sSL https://install.python-poetry.org | python3 - {poetry_version}",
                 ]
-            ).with_env_variable("PATH", "/root/.local/bin:$PATH", expand=True)
+            )
+            .with_env_variable("PATH", "/root/.local/bin:$PATH", expand=True)
         )
-        return ctr
+
+    @function
+    def base(self) -> dagger.Container:
+        return self.ctr
