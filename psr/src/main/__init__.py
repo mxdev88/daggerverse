6bff541822a4599463c69a12f8d9500165fa1f68
mxdev88 <mxdev88@gmail.com>
2025-01-05 04:43:47 +0000

    feat(psr): add global args to version command

--- psr/src/main/__init__.py
+++ psr/src/main/__init__.py
@@ -1,32 +1,37 @@
 """Module for running python-semantic-release (PSR) commands."""
 
+import dataclasses
 import dagger
 from dagger import dag, function, object_type
 
 
 @object_type
 class Psr:
-    @function
-    def base(
-        self,
-        version: str | None = None,
-    ) -> dagger.Container:
-        """Build base environment"""
-        pinned_version = ""
+    ctr: dagger.Container = dataclasses.field(init=False)
+    version: dataclasses.InitVar[str | None] = None
+
+    def __post_init__(self, version: str | None = None):
+        psr_version_args = ""
         if version:
-            pinned_version = f"=={version}"
-        ctr = (
+            psr_version_args = f"--version {version}"
+
+        self.ctr = (
             dag.container()
             .from_("python:3.11-bookworm")
             .with_exec(
                 [
                     "pip",
                     "install",
-                    f"python-semantic-release{pinned_version}",
+                    f"python-semantic-release{psr_version_args}",
                 ]
             )
         )
-        return ctr
+
+
+    @function
+    def container(self) -> dagger.Container:
+        return self.ctr
+
 
     @function
     async def version(
@@ -38,41 +43,53 @@ class Psr:
         vcs_release: bool = False,
         changelog: bool = True,
         build_metadata: str | None = None,
-        version: str | None = None,
+        noop: bool = False,
+        strict: bool = False,
+        config: str | None = None,
     ) -> dagger.Container:
-        args = []
+        global_args = []
+        cmd_args = []
+
+        if noop:
+            global_args.append("--noop")
+        
+        if strict:
+            global_args.append("--strict")
+        
+        if config:
+            global_args.extend(["--config", f"{config}"])
         
         if commit:
-            args.append("--commit")
+            cmd_args.append("--commit")
         else:
-            args.append("--no-commit")
+            cmd_args.append("--no-commit")
 
         if tag:
-            args.append("--tag")
+            cmd_args.append("--tag")
         else:
-            args.append("--no-tag")
+            cmd_args.append("--no-tag")
 
         if push:
-            args.append("--push")
+            cmd_args.append("--push")
         else:
-            args.append("--no-push")
+            cmd_args.append("--no-push")
 
         if changelog:
-            args.append("--changelog")
+            cmd_args.append("--changelog")
         else:
-            args.append("--no-changelog")
+            cmd_args.append("--no-changelog")
 
         if vcs_release:
-            args.append("--vcs-release")
+            cmd_args.append("--vcs-release")
         else:
-            args.append("--no-vcs-release")
+            cmd_args.append("--no-vcs-release")
 
         if build_metadata:
-            args.extend(["--build-metadata", f"{build_metadata}"])
+            cmd_args.extend(["--build-metadata", f"{build_metadata}"])
 
         return (
-            await self.base(version=version)
+            await self.ctr
             .with_directory("/src", source, exclude=[".venv"])
             .with_workdir("/src")
-            .with_exec(["semantic-release", "version", *args])
+            .with_exec(["semantic-release", *global_args, "version", *cmd_args])
         )
